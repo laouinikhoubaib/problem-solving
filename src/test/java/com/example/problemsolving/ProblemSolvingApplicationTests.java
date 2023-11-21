8698167c6023447567f6942cb5234b8255787feb
laouinikhoubaib <73175459+laouinikhoubaib@users.noreply.github.com>
2023-11-21 13:28:59 +0100

    first commit

---
+++ src/test/java/com/example/problemsolving/ProblemSolvingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.problemsolving;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProblemSolvingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
